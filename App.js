e3e0faab24e2187762c74bffd9c9ccfc37174985
Vihan Bansal <vihan.bansal@gmail.com>
2025-03-19 18:35:07 -0400

    modified:   App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler'; // Required for navigation
+import 'react-native-gesture-handler'; // 👈 This must be the FIRST import
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './navigation/AppNavigator';
